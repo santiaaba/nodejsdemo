2a6064ed9d2d41dac1d2a663b8c9641d267e63f8
Santiago Diaz <santiaaba@hotmail.com>
2020-05-21 19:46:02 -0400

    Avanzamos

--- db.js
+++ db.js
@@ -1,19 +1,29 @@
 /* Modulo para la conexion a la base de datos */
 const mysql = require('mysql')
 
+/*
 const db_host = "localhost"
 const db_user = "root"
 const db_pass = "sAn474226"
 const db_db = "nodejsdemo"
+const db_port = "3307"
+*/
 
 class Database {
 	constructor(config){
+		console.log("Conectando SQL:")
+		console.log("host: " + process.env.DB_HOST)
+		console.log("port: " + process.env.DB_PORT)
+		console.log("user: " + process.env.DB_USER)
+		console.log("pass: " + process.env.DB_PASS)
+		console.log("dbname: " + process.env.DB_DATABASE)
 		this.connection = mysql.createPool({
 			connectionLimit: 10,
-			host: db_host,
-			user: db_user,
-			database: db_db,
-			password: db_pass
+			host: process.env.DB_HOST,
+			port: process.env.DB_PORT,
+			user: process.env.DB_USER,
+			password: process.env.DB_PASS,
+			database: process.env.DB_DATABASE
 		})
 	}
 	query(sql){
